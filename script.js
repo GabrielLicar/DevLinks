8e8bd0ef3f486b2e0c2ceb8395a4b478039b1d0c
GabrielLicar <paulogabrielicar@gmail.com>
2023-03-27 11:37:37 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMode(){
+  const html = document.documentElement;
+  html.classList.toggle('light');
+
+  const image = document.querySelector("#profile img");
+
+  if (html.classList.contains('light')){
+    image.setAttribute('src', './assets/avatar-light.png')
+    image.setAttribute('alt', 'avatar no tema branco')
+  }else {
+    image.setAttribute('src', './assets/avatar.png')
+    image.setAttribute("alt", "avatar no tema preto")
+  }
+
+  
+}
